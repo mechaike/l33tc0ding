44e8b575021a352933696eee0f5a05af1c06eabc
Chris Kang <4ckang@gmail.com>
2018-12-08 00:07:16 -0800

    fixed whitespace 686

--- 600/686-Repeated_String_Match.cc
+++ 600/686-Repeated_String_Match.cc
@@ -14,28 +14,28 @@ using namespace std;
 // Note: The length of A and B will be between 1 and 10000.
 
 int repeatedStringMatch(string A, string B) {
-	string a = A;
-	int max_repeats = (B.size()/a.size()) + 2;
+  string a = A;
+  int max_repeats = (B.size()/a.size()) + 2;
 
-	for (int i = 1; i <= max_repeats; ++i)
-	{
-		if(a.size() >= B.size() && a.find(B) != string::npos)
-			return i;
-		else
-			a += A;
-	}	
-	
-	return -1;
+  for (int i = 1; i <= max_repeats; ++i)
+  {
+    if(a.size() >= B.size() && a.find(B) != string::npos)
+      return i;
+    else
+      a += A;
+  } 
+
+  return -1;
 }
 
 int main(){
-	vector<string> t1 = {"abcd", "cdabcdab"};
-	vector<string> t2 = {"abc","cabcabca"};
+  vector<string> t1 = {"abcd", "cdabcdab"};
+  vector<string> t2 = {"abc","cabcabca"};
 
-	int result = repeatedStringMatch(t1[0], t1[1]);
-	cout << result << endl;	//3
+  int result = repeatedStringMatch(t1[0], t1[1]);
+  cout << result << endl; //3
 
-	result = repeatedStringMatch(t2[0], t2[1]);
-	cout << result << endl;	//4
-	return 0;
+  result = repeatedStringMatch(t2[0], t2[1]);
+  cout << result << endl; //4
+  return 0;
 }
